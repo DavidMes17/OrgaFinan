f56eb18222792655e009e58a3e4a8b9dc6251c54
DavidMes17 <davidmes46@gmail.com>
2024-11-21 17:57:00 -0300

    início dos relatórios de urgência

--- src/components/urgencia.component.tsx
+++ src/components/urgencia.component.tsx
@@ -1,14 +1,223 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 
 export default function Urgencia() {
-    return (<>
+    return (<View>
         <View style={styles.titleBack}>
             <Text style={styles.title}>Relatório de urgências</Text>
         </View>
-    </>)
+
+        
+
+            <View style={styles.relatorio}> <ScrollView style={{borderRadius: 10, marginTop: 16}}>
+                <View style={styles.relatorioRetorno}>
+
+                <View style={styles.cardUrgencia}>
+                        <View style={styles.icnUrgencia}>
+                            <Text>Ícone...</Text>
+                        </View>
+                        <View style={styles.titleUrgencia}>
+                            <Text style={styles.titileTxtUrgencia}>amortecedor do carro</Text>
+                            <View style={styles.textUrgencia}>
+                                <Text>R$ 350,17</Text>
+                                <Text>Urgente grau 3</Text>
+                            </View>
+                        </View>
+                    </View>
+
+                    <View style={styles.cardUrgencia}>
+                        <View style={styles.icnUrgencia}>
+                            <Text>Ícone...</Text>
+                        </View>
+                        <View style={styles.titleUrgencia}>
+                            <Text style={styles.titileTxtUrgencia}>amortecedor do carro</Text>
+                            <View style={styles.textUrgencia}>
+                                <Text>R$ 350,17</Text>
+                                <Text>Urgente grau 3</Text>
+                            </View>
+                        </View>
+                    </View>
+
+                    <View style={styles.cardUrgencia}>
+                        <View style={styles.icnUrgencia}>
+                            <Text>Ícone...</Text>
+                        </View>
+                        <View style={styles.titleUrgencia}>
+                            <Text style={styles.titileTxtUrgencia}>amortecedor do carro</Text>
+                            <View style={styles.textUrgencia}>
+                                <Text>R$ 350,17</Text>
+                                <Text>Urgente grau 3</Text>
+                            </View>
+                        </View>
+                    </View>
+
+                    <View style={styles.cardUrgencia}>
+                        <View style={styles.icnUrgencia}>
+                            <Text>Ícone...</Text>
+                        </View>
+                        <View style={styles.titleUrgencia}>
+                            <Text style={styles.titileTxtUrgencia}>amortecedor do carro</Text>
+                            <View style={styles.textUrgencia}>
+                                <Text>R$ 350,17</Text>
+                                <Text>Urgente grau 3</Text>
+                            </View>
+                        </View>
+                    </View>
+
+                    <View style={styles.cardUrgencia}>
+                        <View style={styles.icnUrgencia}>
+                            <Text>Ícone...</Text>
+                        </View>
+                        <View style={styles.titleUrgencia}>
+                            <Text style={styles.titileTxtUrgencia}>amortecedor do carro</Text>
+                            <View style={styles.textUrgencia}>
+                                <Text>R$ 350,17</Text>
+                                <Text>Urgente grau 3</Text>
+                            </View>
+                        </View>
+                    </View>
+
+                    <View style={styles.cardUrgencia}>
+                        <View style={styles.icnUrgencia}>
+                            <Text>Ícone...</Text>
+                        </View>
+                        <View style={styles.titleUrgencia}>
+                            <Text style={styles.titileTxtUrgencia}>amortecedor do carro</Text>
+                            <View style={styles.textUrgencia}>
+                                <Text>R$ 350,17</Text>
+                                <Text>Urgente grau 3</Text>
+                            </View>
+                        </View>
+                    </View>
+
+                    <View style={styles.cardUrgencia}>
+                        <View style={styles.icnUrgencia}>
+                            <Text>Ícone...</Text>
+                        </View>
+                        <View style={styles.titleUrgencia}>
+                            <Text style={styles.titileTxtUrgencia}>amortecedor do carro</Text>
+                            <View style={styles.textUrgencia}>
+                                <Text>R$ 350,17</Text>
+                                <Text>Urgente grau 3</Text>
+                            </View>
+                        </View>
+                    </View>
+
+                    <View style={styles.cardUrgencia}>
+                        <View style={styles.icnUrgencia}>
+                            <Text>Ícone...</Text>
+                        </View>
+                        <View style={styles.titleUrgencia}>
+                            <Text style={styles.titileTxtUrgencia}>amortecedor do carro</Text>
+                            <View style={styles.textUrgencia}>
+                                <Text>R$ 350,17</Text>
+                                <Text>Urgente grau 3</Text>
+                            </View>
+                        </View>
+                    </View>
+
+                    <View style={styles.cardUrgencia}>
+                        <View style={styles.icnUrgencia}>
+                            <Text>Ícone...</Text>
+                        </View>
+                        <View style={styles.titleUrgencia}>
+                            <Text style={styles.titileTxtUrgencia}>amortecedor do carro</Text>
+                            <View style={styles.textUrgencia}>
+                                <Text>R$ 350,17</Text>
+                                <Text>Urgente grau 3</Text>
+                            </View>
+                        </View>
+                    </View>
+
+                    <View style={styles.cardUrgencia}>
+                        <View style={styles.icnUrgencia}>
+                            <Text>Ícone...</Text>
+                        </View>
+                        <View style={styles.titleUrgencia}>
+                            <Text style={styles.titileTxtUrgencia}>amortecedor do carro</Text>
+                            <View style={styles.textUrgencia}>
+                                <Text>R$ 350,17</Text>
+                                <Text>Urgente grau 3</Text>
+                            </View>
+                        </View>
+                    </View>
+
+                    <View style={styles.cardUrgencia}>
+                        <View style={styles.icnUrgencia}>
+                            <Text>Ícone...</Text>
+                        </View>
+                        <View style={styles.titleUrgencia}>
+                            <Text style={styles.titileTxtUrgencia}>amortecedor do carro</Text>
+                            <View style={styles.textUrgencia}>
+                                <Text>R$ 350,17</Text>
+                                <Text>Urgente grau 3</Text>
+                            </View>
+                        </View>
+                    </View>
+
+                    <View style={styles.cardUrgencia}>
+                        <View style={styles.icnUrgencia}>
+                            <Text>Ícone...</Text>
+                        </View>
+                        <View style={styles.titleUrgencia}>
+                            <Text style={styles.titileTxtUrgencia}>amortecedor do carro</Text>
+                            <View style={styles.textUrgencia}>
+                                <Text>R$ 350,17</Text>
+                                <Text>Urgente grau 3</Text>
+                            </View>
+                        </View>
+                    </View>
+
+                    <View style={styles.cardUrgencia}>
+                        <View style={styles.icnUrgencia}>
+                            <Text>Ícone...</Text>
+                        </View>
+                        <View style={styles.titleUrgencia}>
+                            <Text style={styles.titileTxtUrgencia}>amortecedor do carro</Text>
+                            <View style={styles.textUrgencia}>
+                                <Text>R$ 350,17</Text>
+                                <Text>Urgente grau 3</Text>
+                            </View>
+                        </View>
+                    </View>
+
+                    <View style={styles.cardUrgencia}>
+                        <View style={styles.icnUrgencia}>
+                            <Text>Ícone...</Text>
+                        </View>
+                        <View style={styles.titleUrgencia}>
+                            <Text style={styles.titileTxtUrgencia}>amortecedor do carro</Text>
+                            <View style={styles.textUrgencia}>
+                                <Text>R$ 350,17</Text>
+                                <Text>Urgente grau 3</Text>
+                            </View>
+                        </View>
+                    </View>
+
+                    <View style={styles.cardUrgencia}>
+                        <View style={styles.icnUrgencia}>
+                            <Text>Ícone...</Text>
+                        </View>
+                        <View style={styles.titleUrgencia}>
+                            <Text style={styles.titileTxtUrgencia}>amortecedor do carro</Text>
+                            <View style={styles.textUrgencia}>
+                                <Text>R$ 350,17</Text>
+                                <Text>Urgente grau 3</Text>
+                            </View>
+                        </View>
+                    </View>
+
+                </View>
+                </ScrollView></View>
+
+</View>)
 }
 
 const styles = StyleSheet.create({
+    title: {
+        fontSize: 48,
+        fontWeight: 'bold',
+        color: '#fff',
+    },
     titleBack: {
         backgroundColor: '#4FA8FF',
         paddingVertical: 4,
@@ -17,9 +226,49 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         borderRadius: 40,
     },
-    title: {
-        fontSize: 48,
-        fontWeight: 'bold',
-        color: '#fff',
+    relatorio: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        marginHorizontal: 8,
+        height: 250,
     },
+    relatorioRetorno: {
+        backgroundColor: '#B0A4FF',
+        flex: 0.8,
+        marginTop: 16,
+        borderRadius: 10,
+        alignItems: 'center',
+        padding: 8,
+    },
+    cardUrgencia: {
+        flexDirection: 'row',
+        backgroundColor: '#D9D9D9',
+        width: '100%',
+        padding: 8,
+        borderRadius: 15,
+        justifyContent: 'space-between',
+        marginVertical: 4,
+    },
+    icnUrgencia: {
+        backgroundColor: 'blue',
+        borderRadius: 100,
+        aspectRatio: 1,
+        alignContent: 'center',
+        justifyContent: 'center',
+    },
+    titleUrgencia: {
+        justifyContent: 'center',
+        alignContent: 'center',
+        textAlign: 'center',
+    },
+    titileTxtUrgencia: {
+        textTransform: 'uppercase',
+        fontWeight: 'bold',
+        fontSize: 15,
+    },
+    textUrgencia: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    }
 })
